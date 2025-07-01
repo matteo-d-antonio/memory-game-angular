6302b3eab7847636feb2226495d95310bb7e64b2
matteo-d-antonio <m.dantonio@elitesoftwarehouse.com>
2025-07-01 16:57:55 +0200

    aggiunta visualizzazione card e funzione reset per mescolare

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
@@ -6,13 +6,19 @@ import { CommonModule } from '@angular/common';
   selector: 'app-root',
   imports: [RouterOutlet , CommonModule,],
   template: `
-  <div class="container" style="font-size:40px">
-  <span class="title" >Memory game</span>
-  <span><button class="btn btn-primary">PLAY</button></span>
-  </div>
+  <div>
+    <div class="container" style="font-size:40px">
+      <span class="title" >Memory game</span>
+      <span><button class="btn btn-outline-danger" (click)="shuffleCards()">RESET</button></span>
+    </div>
 
-  <div class="card">
+    <div class="card-container">
+      <li *ngFor="let card of cards()" class="ms-card-item">
+        {{ card.name }}
+      </li>
+    </div>
   </div>
+  
     
     <router-outlet />
   `,
@@ -24,18 +30,52 @@ import { CommonModule } from '@angular/common';
       margin: 0 auto;
       padding: 50px;
     }
+    .ms-card-item {
+      display: inline-block;
+      width: 100px;
+      height: 100px;
+      background-color:rgba(255, 255, 255, 0.1);
+      border: 1px solid #ccc;
+      border-radius: 8px;
+      text-align: center;
+      line-height: 100px;
+      margin: 10px;
+      font-size: 2em;
+      cursor: pointer;
+      color: white;
+    }
+    .card-container {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      max-width: 500px;
+      margin: 0 auto;
+    } 
+      
+}
   `,
 })
 export class App {
   protected title = 'angular-memory-game';
   products: Product[] = [
-    {id: 1, name: '✔️'},
-    {id: 2, name: '✖️'},
-    {id: 3, name: '➗'},
-    {id: 4, name: '➕'},
-    {id: 5, name: '➖'},
-    {id: 6, name: '🟰'},
+    {id: 1, name: '🐷'},
+    {id: 2, name: '🐮'},
+    {id: 3, name: '🐔'},
+    {id: 4, name: '🐰'},
+    {id: 5, name: '🐸'},
+    {id: 6, name: '🐵'},
   ]
+
+  cards = signal<Product[]>([]);
+
+
+  shuffleCards() {
+    const duplicated = [...this.products, ...this.products];
+    const shuffled = duplicated
+    .map((card, index) => ({ ...card, id: index })) // assegna id unici
+    .sort(() => Math.random() - 0.5);
+    this.cards.set(shuffled);
+  }
 
 }
 type Product = {
