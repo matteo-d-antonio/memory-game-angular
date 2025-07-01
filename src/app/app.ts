ac4320079053502e93f3d3c410c57a71c153b780
matteo-d-antonio <m.dantonio@elitesoftwarehouse.com>
2025-07-01 11:25:36 +0200

    init commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   template: `
     <h1>Welcome to {{title}}!</h1>
-
+    <h2>Memory Game</h2>
     <router-outlet />
   `,
   styles: [],
