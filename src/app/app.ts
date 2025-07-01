deb80cc13aa426164de7698513441114ab2799bc
matteo-d-antonio <m.dantonio@elitesoftwarehouse.com>
2025-07-01 12:13:33 +0200

    modifiche allo stile

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , CommonModule],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-    <h2>Memory Game</h2>
+  <div class="container" style="font-size:40px">
+  <span class="title" >Memory game</span>
+  <span><button class="btn btn-primary">PLAY</button></span>
+  </div>
+
+  <div class="card">
+  </div>
+    
     <router-outlet />
   `,
-  styles: [],
+  styles: `
+    .container {
+      display: flex;
+      justify-content: space-between;
+      max-width: 600px;
+      margin: 0 auto;
+      padding: 50px;
+    }
+  `,
 })
 export class App {
   protected title = 'angular-memory-game';
